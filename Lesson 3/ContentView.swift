d45ad85c85abb9ef84667d34fdd728746dce4e6b
ganudoomer <ganuiscool@gmail.com>
2021-08-23 23:01:44 +0530

    Initial Commit

---
+++ Lesson 3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lesson 3
+//
+//  Created by Sree on 23/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lesson 3/Lesson_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson_3App.swift
+//  Lesson 3
+//
+//  Created by Sree on 23/08/21.
+//
+
+import SwiftUI
+
+@main
+struct Lesson_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
